9532f657e727adce4b80bcbaa51fa26a23fc612b
Jose Hernandez Jaimes <hedzjames@gmail.com>
2017-07-05 10:35:05 -0500

    Update App.java

--- src/main/java/com/beeva/banco/BancoApp/App.java
+++ src/main/java/com/beeva/banco/BancoApp/App.java
@@ -11,8 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Operaciones op = new Operaciones();
-        System.out.println("Operaciones : " + op.suma(30, 5)); 
+       
         
     }
 }
